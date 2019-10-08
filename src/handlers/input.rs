804764df8807a6510a4b8aa64683b417c15e67f1
Fangyi Zhou <me@fangyi.io>
2019-10-08 21:33:15 +0100

    input: Take care of index and width separately
    This resolves #26, where inserting into a string outside character
    boundary leads to a panic (ref:
    https://doc.rust-lang.org/std/string/struct.String.html#method.insert)
    ""Panics if idx is larger than the String's length, or
    if it does not lie on a char boundary.""
    
    We use `char_indices` to find the correct position to insert into
    and `unicode_width` crate to find out the width of characters.

--- src/handlers/input.rs
+++ src/handlers/input.rs
@@ -1,28 +1,42 @@
+extern crate unicode_width;
+
 use super::super::app::{ActiveBlock, App, RouteId};
 use std::convert::TryInto;
 use termion::event::Key;
+use unicode_width::{UnicodeWidthChar, UnicodeWidthStr};
 
 // Handle events when the search input block is active
 pub fn handler(key: Key, app: &mut App) {
     match key {
         Key::Ctrl('u') => {
             app.input = String::new();
+            app.input_idx = 0;
             app.input_cursor_position = 0;
         }
         Key::Ctrl('e') => {
-            app.input_cursor_position = app.input.len().try_into().unwrap();
+            app.input_idx = app.input.len();
+            app.input_cursor_position = UnicodeWidthStr::width(app.input.as_str())
+                .try_into()
+                .unwrap();
         }
         Key::Ctrl('a') => {
+            app.input_idx = 0;
             app.input_cursor_position = 0;
         }
         Key::Left => {
-            if !app.input.is_empty() && app.input_cursor_position > 0 {
-                app.input_cursor_position -= 1;
+            if !app.input.is_empty() && app.input_idx > 0 {
+                let last_c = app.input.chars().nth(app.input_idx - 1).unwrap();
+                app.input_idx -= 1;
+                let width: u16 = UnicodeWidthChar::width(last_c).unwrap().try_into().unwrap();
+                app.input_cursor_position -= width;
             }
         }
         Key::Right => {
             if app.input_cursor_position < app.input.len().try_into().unwrap() {
-                app.input_cursor_position += 1;
+                let next_c = app.input.chars().nth(app.input_idx).unwrap();
+                app.input_idx += 1;
+                let width: u16 = UnicodeWidthChar::width(next_c).unwrap().try_into().unwrap();
+                app.input_cursor_position += width;
             }
         }
         Key::Esc => {
@@ -94,15 +108,23 @@ pub fn handler(key: Key, app: &mut App) {
             }
         }
         Key::Char(c) => {
-            app.input
-                .insert(app.input_cursor_position.try_into().unwrap(), c);
-            app.input_cursor_position += 1;
+            let (insert_idx, _) = app
+                .input
+                .char_indices()
+                .nth(app.input_idx)
+                .unwrap_or((app.input.len(), ' '));
+            app.input.insert(insert_idx, c);
+            app.input_idx += 1;
+            let width: u16 = UnicodeWidthChar::width(c).unwrap().try_into().unwrap();
+            app.input_cursor_position += width;
         }
         Key::Backspace => {
-            if !app.input.is_empty() && app.input_cursor_position > 0 {
-                app.input
-                    .remove((app.input_cursor_position - 1).try_into().unwrap());
-                app.input_cursor_position -= 1;
+            if !app.input.is_empty() && app.input_idx > 0 {
+                let (remove_idx, last_c) = app.input.char_indices().nth(app.input_idx - 1).unwrap();
+                app.input_idx -= 1;
+                app.input.remove(remove_idx);
+                let width: u16 = UnicodeWidthChar::width(last_c).unwrap().try_into().unwrap();
+                app.input_cursor_position -= width;
             }
         }
         _ => {}
