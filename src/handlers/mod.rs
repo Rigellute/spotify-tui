55bbe8f642353b08638598a3798048db5e8e0063
Rigellute <rigellute@gmail.com>
2019-10-13 13:11:23 +0100

    Move global events handler into `handlers` dir

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -17,11 +17,84 @@ mod search_results;
 mod select_device;
 mod track_table;
 
-use super::app::{ActiveBlock, App};
+use super::app::{ActiveBlock, App, SearchResultBlock};
 use termion::event::Key;
 
-pub fn handle_app(app: &mut App, key: Key) {
-    // Match events for different app states
+pub use input::handler as input_handler;
+
+pub fn handle_app(key: Key, app: &mut App) {
+    // First handle any global events and then move to block events
+    match key {
+        Key::Esc => match app.get_current_route().active_block {
+            ActiveBlock::SearchResultBlock => {
+                app.search_results.selected_block = SearchResultBlock::Empty;
+            }
+            ActiveBlock::Error => {
+                app.pop_navigation_stack();
+            }
+            _ => {
+                app.set_current_route_state(Some(ActiveBlock::Empty), None);
+            }
+        },
+        // Jump to currently playing album
+        Key::Char('a') => {
+            if let Some(current_playback_context) = &app.current_playback_context {
+                if let Some(full_track) = &current_playback_context.item.clone() {
+                    app.get_album_tracks(full_track.album.clone());
+                }
+            };
+        }
+        // Jump to currently playing artist's album list.
+        // NOTE: this only finds the first artist of the song and jumps to their albums
+        Key::Char('A') => {
+            if let Some(current_playback_context) = &app.current_playback_context {
+                if let Some(playing_item) = &current_playback_context.item.clone() {
+                    if let Some(artist) = playing_item.artists.first() {
+                        if let Some(artist_id) = &artist.id {
+                            app.get_artist_albums(artist_id, &artist.name);
+                        }
+                    }
+                }
+            };
+        }
+        Key::Char('d') => {
+            app.handle_get_devices();
+        }
+        // Press space to toggle playback
+        Key::Char(' ') => {
+            app.toggle_playback();
+        }
+        Key::Char('<') => {
+            app.seek_backwards();
+        }
+        Key::Char('>') => {
+            app.seek_forwards();
+        }
+        Key::Char('n') => {
+            app.next_track();
+        }
+        Key::Char('p') => {
+            app.previous_track();
+        }
+        Key::Char('?') => {
+            app.set_current_route_state(Some(ActiveBlock::HelpMenu), None);
+        }
+
+        Key::Ctrl('s') => {
+            app.shuffle();
+        }
+        Key::Ctrl('r') => {
+            app.repeat();
+        }
+        Key::Char('/') => {
+            app.set_current_route_state(Some(ActiveBlock::Input), Some(ActiveBlock::Input));
+        }
+        _ => handle_block_events(key, app),
+    }
+}
+
+// Handle events for the current active block
+fn handle_block_events(key: Key, app: &mut App) {
     let current_route = app.get_current_route();
     match current_route.active_block {
         ActiveBlock::Artist => {
