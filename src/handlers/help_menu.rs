cd6e225342022e6551a9828d091b27ea3365fab4
PedroHVA <pedro.valentealves@connect.qut.edu.au>
2020-02-19 17:36:53 -0300

    Add up/down arrows as navigation options in the help menu

--- src/handlers/help_menu.rs
+++ src/handlers/help_menu.rs
@@ -1,17 +1,37 @@
 use crate::{app::App, event::Key};
+use super::common_key_events;
+
+#[derive(PartialEq)]
+enum Direction {
+    UP,
+    DOWN,
+}
 
 pub fn handler(key: Key, app: &mut App) {
     match key {
+        k if common_key_events::down_event(k) => {
+            move_page(Direction::DOWN, app);
+        }
+        k if common_key_events::up_event(k) => {
+            move_page(Direction::UP, app);
+        }
         Key::Ctrl('d') => {
-            app.help_menu_page += 1;
-            app.calculate_help_menu_offset();
+            move_page(Direction::DOWN, app);
         }
         Key::Ctrl('u') => {
-            if app.help_menu_page > 0 {
-                app.help_menu_page -= 1;
-                app.calculate_help_menu_offset();
-            }
+            move_page(Direction::UP, app);
         }
         _ => {}
     };
+}
+
+fn move_page(direction: Direction, app: &mut App) {
+    if direction == Direction::UP {
+        if app.help_menu_page > 0 {
+            app.help_menu_page -= 1;
+        }
+    } else if direction == Direction::DOWN {
+        app.help_menu_page += 1;
+    }
+    app.calculate_help_menu_offset();
 }
