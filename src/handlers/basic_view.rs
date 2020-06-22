1072df58e801e11095608cf48061c2e9d8319915
RicardoHE97 <rholguin@protonmail.com>
2020-06-22 15:04:49 -0700

    Add feature to like a song from basic view
    Add the feature to like a song with the 's' key when you are in the basic view.

--- src/handlers/basic_view.rs
+++ src/handlers/basic_view.rs
@@ -1,3 +1,16 @@
-use crate::{app::App, event::Key};
+use crate::{app::App, event::Key, network::IoEvent};
 
-pub fn handler(_key: Key, _app: &mut App) {}
+pub fn handler(key: Key, app: &mut App) {
+  match key {
+    Key::Char('s') => {
+      if let Some(playing_context) = &app.current_playback_context {
+        if let Some(track) = &playing_context.clone().item {
+          if let Some(id) = &track.id {
+            app.dispatch(IoEvent::ToggleSaveTrack(id.to_string()));
+          }
+        }
+      }
+    }
+    _ => {}
+  };
+}
