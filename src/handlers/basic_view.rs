ee9c79e998dbfc301ef4fc8cbd2152471223c942
RicardoHE97 <rholguin@protonmail.com>
2020-06-22 22:01:25 -0700

    Reformat code for basic_view.rs that fix a common mistake

--- src/handlers/basic_view.rs
+++ src/handlers/basic_view.rs
@@ -1,16 +1,13 @@
 use crate::{app::App, event::Key, network::IoEvent};
 
 pub fn handler(key: Key, app: &mut App) {
-  match key {
-    Key::Char('s') => {
-      if let Some(playing_context) = &app.current_playback_context {
-        if let Some(track) = &playing_context.clone().item {
-          if let Some(id) = &track.id {
-            app.dispatch(IoEvent::ToggleSaveTrack(id.to_string()));
-          }
+  if let Key::Char('s') = key {
+    if let Some(playing_context) = &app.current_playback_context {
+      if let Some(track) = &playing_context.clone().item {
+        if let Some(id) = &track.id {
+          app.dispatch(IoEvent::ToggleSaveTrack(id.to_string()));
         }
       }
     }
-    _ => {}
-  };
+  }
 }
