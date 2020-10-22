ed0bf031f81ee348cab0da03939cb3a6848a669b
May <mehrabhoque@gmail.com>
2020-10-22 06:47:42 -0400

    Support searching by Spotify share URLs and URIs analogously to the desktop client (#623)
    * Generalize input processing to both URLs and URIs
    
    Unfortunately, I would have preferred the small refactor of adding
    process_input() to be in a separate commit, but I quite genuinely just
    forgot to commit my work, so these guys are mushed together here.
    
    * Add the boilerplate code for supporting tracks, playlists and podcasts
    
    * Introduce a spotify_resource_id() closure to reduce some duplication
    
    * Support searching playlists by URL/Spotify-URI
    
    * Support searching shows by URL/Spotify-URI
    
    * Support searching individual tracks by URL/Spotify-URI
    
    * Add initial parsing tests and required refactors
    
    * Add full suite of happy path test cases
    
    * Verify that we fail to match on invalid strings
    
    * Remove debug prints
    
    * Correct seek to track index on track search
    
    Albeit, not very cleanly. Want to try getting it nicer before I put this
    up...
    
    * Handle query parameters in URIs
    
    * Always push a GetPlaylist to the navigation stack to avoid UI inconsistency
    
    * Clear the playlist selection no matter what kind of search we do
    
    * Remove debug prints
    
    * Remove redundant clone
    
    * Prefer unwrap_or_else()

--- src/network.rs
+++ src/network.rs
@@ -76,6 +76,7 @@ pub enum IoEvent {
   UserArtistFollowCheck(Vec<String>),
   GetAlbum(String),
   TransferPlaybackToDevice(String),
+  GetAlbumForTrack(String),
   CurrentUserSavedTracksContains(Vec<String>),
   GetShowEpisodes(String),
   AddItemToQueue(String),
@@ -260,6 +261,9 @@ impl<'a> Network<'a> {
       IoEvent::TransferPlaybackToDevice(device_id) => {
         self.transfert_playback_to_device(device_id).await;
       }
+      IoEvent::GetAlbumForTrack(track_id) => {
+        self.get_album_for_track(track_id).await;
+      }
       IoEvent::Shuffle(shuffle_state) => {
         self.shuffle(shuffle_state).await;
       }
@@ -377,9 +381,7 @@ impl<'a> Network<'a> {
 
       let mut app = self.app.lock().await;
       app.playlist_tracks = Some(playlist_tracks);
-      if app.get_current_route().id != RouteId::TrackTable {
-        app.push_navigation_stack(RouteId::TrackTable, ActiveBlock::TrackTable);
-      };
+      app.push_navigation_stack(RouteId::TrackTable, ActiveBlock::TrackTable);
     };
   }
 
@@ -1248,6 +1250,39 @@ impl<'a> Network<'a> {
         app.selected_album_full = Some(selected_album);
         app.album_table_context = AlbumTableContext::Full;
         app.push_navigation_stack(RouteId::AlbumTracks, ActiveBlock::AlbumTracks);
+      }
+      Err(e) => {
+        self.handle_error(anyhow!(e)).await;
+      }
+    }
+  }
+
+  async fn get_album_for_track(&mut self, track_id: String) {
+    match self.spotify.track(&track_id).await {
+      Ok(track) => {
+        // It is unclear when the id can ever be None, but perhaps a track can be album-less. If
+        // so, there isn't much to do here anyways, since we're looking for the parent album.
+        let album_id = match track.album.id {
+          Some(id) => id,
+          None => return,
+        };
+
+        if let Ok(album) = self.spotify.album(&album_id).await {
+          // The way we map to the UI is zero-indexed, but Spotify is 1-indexed.
+          let zero_indexed_track_number = track.track_number - 1;
+          let selected_album = SelectedFullAlbum {
+            album,
+            // Overflow should be essentially impossible here, so we prefer the cleaner 'as'.
+            selected_index: zero_indexed_track_number as usize,
+          };
+
+          let mut app = self.app.lock().await;
+
+          app.selected_album_full = Some(selected_album.clone());
+          app.saved_album_tracks_index = selected_album.selected_index;
+          app.album_table_context = AlbumTableContext::Full;
+          app.push_navigation_stack(RouteId::AlbumTracks, ActiveBlock::AlbumTracks);
+        }
       }
       Err(e) => {
         self.handle_error(anyhow!(e)).await;
