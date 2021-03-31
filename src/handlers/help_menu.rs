073a8e5711d0fa1be338663895948d2d523ace63
Alexander Sieg <alex@xanderio.de>
2021-03-31 17:13:43 +0200

    fix: clippy and compiler warnings
    For some time now the compiler and clippy where warning about some minor
    code style issue. This commit fixes these issues.

--- src/handlers/help_menu.rs
+++ src/handlers/help_menu.rs
@@ -3,34 +3,34 @@ use crate::{app::App, event::Key};
 
 #[derive(PartialEq)]
 enum Direction {
-  UP,
-  DOWN,
+  Up,
+  Down,
 }
 
 pub fn handler(key: Key, app: &mut App) {
   match key {
     k if common_key_events::down_event(k) => {
-      move_page(Direction::DOWN, app);
+      move_page(Direction::Down, app);
     }
     k if common_key_events::up_event(k) => {
-      move_page(Direction::UP, app);
+      move_page(Direction::Up, app);
     }
     Key::Ctrl('d') => {
-      move_page(Direction::DOWN, app);
+      move_page(Direction::Down, app);
     }
     Key::Ctrl('u') => {
-      move_page(Direction::UP, app);
+      move_page(Direction::Up, app);
     }
     _ => {}
   };
 }
 
 fn move_page(direction: Direction, app: &mut App) {
-  if direction == Direction::UP {
+  if direction == Direction::Up {
     if app.help_menu_page > 0 {
       app.help_menu_page -= 1;
     }
-  } else if direction == Direction::DOWN {
+  } else if direction == Direction::Down {
     app.help_menu_page += 1;
   }
   app.calculate_help_menu_offset();
