de9516a1cf1e70a45f798725a696710d5df55cf9
Giorgioskij <giorgiopuntostrano@gmail.com>
2021-12-02 15:55:55 +0100

    added help entry for the new shortcut

--- src/ui/help.rs
+++ src/ui/help.rs
@@ -158,6 +158,11 @@ pub fn get_help_docs(key_bindings: &KeyBindings) -> Vec<Vec<String>> {
       String::from("General"),
     ],
     vec![
+      String::from("Show lyrics for the current song"),
+      key_bindings.show_lyrics.to_string(),
+      String::from("General"),
+    ],
+    vec![
       String::from("Enter hover mode"),
       String::from("<Esc>"),
       String::from("Selected block"),
