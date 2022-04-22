10ad0b6312de25bf48889e3cab3e3c44946d137d
luozx <luozx@umich.edu>
2022-04-22 12:12:26 -0400

    new test

---
+++ src/cli/test_handle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+spt play --name "initial d" --playlist --random
+spt play --name "roddy" --liked --random
+spt play --name "roddy" --playlist --random
+spt play --name "initial d" --liked  --random 
+spt play --name "liked" --liked --random
+spt list --liked
+spt play --name "recent" --liked  --random
+spt play --name "initial d" --playlist
+spt play --name "initial d" --liked  --random  
+spt play --name "initial d" --liked
+spt play --name "liked" --liked
